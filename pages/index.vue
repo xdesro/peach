ee3260c1fdd029aa11e09c6a4af894124e1dd683
Henry Desroches <yo@henry.codes>
2019-05-06 20:12:40 -0600

    👹  - Begin distortion effects, clean up minor line-height thing

--- pages/index.vue
+++ pages/index.vue
@@ -48,6 +48,24 @@
         </li>
       </ul>
     </section>
+    <svg class="displacement-map">
+      <defs>
+        <filter id="turbulence" x="-10%" y="0%" width="120%" height="100%">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.002"
+            result="fracNoise"
+          />
+          <feDisplacementMap
+            in="SourceGraphic"
+            in2="blurredFracNoise"
+            scale="50"
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        </filter>
+      </defs>
+    </svg>
   </div>
 </template>
 
