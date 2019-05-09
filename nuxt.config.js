bd95fbc8b2c7785f5a61dcdd41f779a04d93616b
Henry Desroches <yo@henry.codes>
2019-05-09 07:33:36 -0700

    📈  - add opengraph meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,43 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
       { name: 'theme-color', content: '#FDB4AA' },
-      { name: 'msapplication-TileColor', content: '#FDB4AA' }
+      { name: 'msapplication-TileColor', content: '#FDB4AA' },
+      { name: 'image', content: 'https://henry.codes/opengraph.jpg' },
+      { itemprop: 'name', content: 'Creative Developer // Henry Desroches' },
+      {
+        itemprop: 'description',
+        content:
+          'Personal site and portfolio of UX engineer and prolific twitter user xdesro.'
+      },
+      { itemprop: 'image', content: 'https://henry.codes/opengraph.jpg' },
+
+      { name: 'twitter:card', content: 'summary' },
+      {
+        name: 'twitter:title',
+        content: 'Creative Developer // Henry Desroches'
+      },
+      {
+        name: 'twitter:description',
+        content:
+          'Personal site and portfolio of UX engineer and prolific twitter user xdesro.'
+      },
+      { name: 'twitter:site', content: '@xdesro' },
+      { name: 'twitter:creator', content: '@xdesro' },
+      { name: 'twitter:image:src', content: 'https://henry.codes/twitter.jpg' },
+      { name: 'og:title', content: 'Creative Developer // Henry Desroches' },
+      {
+        name: 'og:description',
+        content:
+          'Personal site and portfolio of UX engineer and prolific twitter user xdesro.'
+      },
+      { name: 'og:image', content: 'https://henry.codes/opengraph.jpg' },
+      { name: 'og:url', content: 'https://henry.codes' },
+      {
+        name: 'og:site_name',
+        content: 'Creative Developer // Henry Desroches'
+      },
+      { name: 'og:locale', content: 'en_US' },
+      { name: 'og:type', content: 'website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
